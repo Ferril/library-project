e3fb2ecb2fe367126180ab3fdb60cb0fbdd579e5
ferril <ferril.darkdiver@gmail.com>
2019-11-26 08:11:59 +0300

    Corrected some code after linter checking

--- library/library/settings.py
+++ library/library/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'change_me')
 
-DEBUG = int(os.environ.get('DEBUG', default=0))
+DEBUG = int(os.environ.get('DEBUG', default=False))
 
 DJANGO_ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS')
 ALLOWED_HOSTS = DJANGO_ALLOWED_HOSTS.split(' ') if DJANGO_ALLOWED_HOSTS else []
